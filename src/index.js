6b0ba1ced0f594e03d025a82a3e3e8dd6a51c7a5
”Denis <deniska.vla@gmail.com>
2021-02-23 16:34:04 +0200

    Customization prettierrc

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <span>Hi </span>
+      <span>Hi</span>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
