70a68cb4011c22e2fdc53af3cd6f43c8feaa7c77
”Denis <deniska.vla@gmail.com>
2021-02-24 21:45:37 +0200

    Routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { Home } from "./auth/components/Home";
+import { Login } from "./auth/components/Login";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <span>Hi</span>
-      </BrowserRouter>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
