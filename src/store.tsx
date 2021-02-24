b071e9a9df6e5abe5b8ccf8f94387557a72d89c2
”Denis <deniska.vla@gmail.com>
2021-02-24 16:27:27 +0200

    authSlice

--- src/store.tsx
+++ src/store.tsx
@@ -1,5 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { auth } from "./auth/slice/auth";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ auth });
 
 export const store = configureStore({ reducer: rootReducer });
