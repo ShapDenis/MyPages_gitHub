49c1a1858a8abb2d7d6e3b249cafc54431bc0a35
”Denis <deniska.vla@gmail.com>
2021-02-26 21:49:05 +0200

    isAuth add Selectors

--- src/AnonymousRouting.tsx
+++ src/AnonymousRouting.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import { FunctionComponent } from "react";
-import { isAuthSelector } from "../src/auth/selectors/isAuthSelector";
+import { useSelector } from "react-redux";
+import { isAuthSelector } from "./auth/selectors/isAuthSelector";
 
 export const AnonymousRouting: FunctionComponent<Props> = ({
   path,
   component,
   ...rest
 }) => {
-  if (isAuthSelector) {
+  const isAuth = useSelector(isAuthSelector);
+
+  if (isAuth) {
     return <Redirect to={"/"} />;
   } else {
     return <Route {...rest} path={path} component={component} />;
--- src/AuthorizedRouting.tsx
+++ src/AuthorizedRouting.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import { FunctionComponent } from "react";
-import { isAuthSelector } from "../src/auth/selectors/isAuthSelector";
+import { useSelector } from "react-redux";
+import { isAuthSelector } from "./auth/selectors/isAuthSelector";
 export const AuthorizedRouting: FunctionComponent<Props> = ({
   path,
   component,
   ...rest
 }) => {
-  if (isAuthSelector) {
+  const isAuth = useSelector(isAuthSelector);
+
+  if (isAuth) {
     return <Route {...rest} path={path} component={component} />;
   } else {
     return <Redirect to={"/login"} />;
