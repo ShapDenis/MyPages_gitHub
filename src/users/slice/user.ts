39aa1450c38c905194bfe86552d32845cab2ea4b
”Denis <deniska.vla@gmail.com>
2021-07-08 11:02:56 +0300

    fix Users

--- src/users/slice/user.ts
+++ src/users/slice/user.ts
@@ -7,17 +7,15 @@ import { feature } from "../constants";
 import { logoutAsync } from "../../auth/slice/auth";
 import { store } from "../../store";
 
-type users = { id: string; name: string };
-export const usersAdapter = createEntityAdapter<users>({
+type User = { id: string; name: string };
+export const usersAdapter = createEntityAdapter<User>({
   selectId: (users) => users.id,
   sortComparer: (a, b) => a.name.localeCompare(b.name),
 });
 
 type RootState = ReturnType<typeof store.getState>;
 const selectState = (state: RootState) => state.users;
-console.log(selectState);
 export const { selectAll } = usersAdapter.getSelectors(selectState);
-
 export const getUsers = createAsyncThunk(`${feature}/getUsers`, async () => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
   const data = await response.json();
@@ -32,7 +30,7 @@ export const usersSlice = createSlice({
     [getUsers.fulfilled.type]: (state, action) => {
       usersAdapter.setAll(state, action.payload);
     },
-    [logoutAsync.fulfilled.type]: (selectAll, action) => {
+    [logoutAsync.fulfilled.type]: (state, action) => {
       return [];
     },
   },
