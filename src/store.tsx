e0386d46d3ab77afb57bc0af69196ed4be8e659c
”Denis <deniska.vla@gmail.com>
2021-07-08 11:40:06 +0300

    fix Users

--- src/store.tsx
+++ src/store.tsx
@@ -5,3 +5,5 @@ import { users } from "./users/slice/user";
 const rootReducer = combineReducers({ auth, users });
 
 export const store = configureStore({ reducer: rootReducer });
+
+export type RootState = ReturnType<typeof store.getState>;
